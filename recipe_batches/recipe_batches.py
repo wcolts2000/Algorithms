60b3a75803319474db5e5afac6a8db7e4aa4204b
wcolts2000 <wcolts2000@gmail.com>
2019-07-17 13:41:46 -0700

    completed brute force recipes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -9,6 +9,16 @@ def recipe_batches(recipe, ingredients):
     # check that values of available ingredients are greater than the required ingredients
     #    if so get the floor division of the ingredients and the recipe
     #        if that is smaller than current batches value set batches to newest number
+    for key in recipe.keys():
+        if ingredients.get(key) == None:
+            return 0
+        possible_batches = ingredients.get(key) // recipe.get(key)
+        if possible_batches == 0:
+            return 0
+        elif batches == 0:
+            batches = possible_batches
+        elif possible_batches < batches:
+            batches = possible_batches
 
     return batches
 
