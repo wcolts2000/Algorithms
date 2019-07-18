4a309f307b4450efb9ddf90ce4defe6d3c01aaaa
wcolts2000 <wcolts2000@gmail.com>
2019-07-18 14:03:36 -0700

    added recursive option to rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python
-
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    rounds_left = n
+    # Create a list of choices
+    choices = ['rock', 'paper', 'scissors']
+    # create a place to store the answers
+    answer = []
+    plays = []
+    # if n <= 0:
+    #     return answer
+
+    def recurse(rounds_left, plays):
+        # Start base case
+        if rounds_left < 1:
+            answer.append(plays)
+            plays = []
+            return answer
+        else:
+            for i in range(len(choices)):
+                current = choices[i]
+                # print('current', current, ' Plays: ', plays, type(plays))
+                # plays.append(current)
+                recurse(rounds_left - 1, plays + [current])
+    recurse(rounds_left, plays)
+    # print(answer)
+    return answer
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
